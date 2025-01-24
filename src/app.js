cc51891f490ba555598d1e5fa3f6cb08f7ade35a
Edwin Allauca <edwindavid21v@gmail.com>
2025-01-23 21:46:17 -0500

    feat: :sparkles: Add new features
    Add new lines in app js and use cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import CORS
 const inventoryRoutes = require('./routes/inventoryRoutes');
 
 const app = express();
 
+app.use(cors()); // Enable CORS middleware
 app.use(bodyParser.json()); // Make sure the server can interpret JSON
 
 app.use('/api/inventory', inventoryRoutes);
@@ -12,6 +14,7 @@ const PORT = process.env.PORT || 3010;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+// Ensure BigInt can be serialized as JSON
 BigInt.prototype.toJSON = function () {
   return this.toString(); // Convert BigInt to String for JSON serialization
 };
