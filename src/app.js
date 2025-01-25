9b2ff9b3c2497f528cafccebf79cf3d2a2914dca
Edwin Allauca <edwindavid21v@gmail.com>
2025-01-25 12:30:28 -0500

    feat: :wrench: Add files for swagger
    Add files for swagger and update app.js file

--- src/app.js
+++ src/app.js
@@ -1,20 +1,34 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Import CORS
+const cors = require('cors');
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
 const inventoryRoutes = require('./routes/inventoryRoutes');
+
+// Load documentation Swagger
+const swaggerDocument = YAML.load('./src/docs/swagger.yaml');
 
 const app = express();
 
-app.use(cors()); // Enable CORS middleware
-app.use(bodyParser.json()); // Make sure the server can interpret JSON
+app.use(cors());  // Enable CORS
+app.use(bodyParser.json());  // Make sure the server interprets JSON
 
+// Route for see Swagger UI
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// Routes of inventary
 app.use('/api/inventory', inventoryRoutes);
 
 const PORT = process.env.PORT || 3010;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Inventory Update service running on port ${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+});
 
-// Ensure BigInt can be serialized as JSON
+
+// Ensure BigInt can be serialized to JSON
 BigInt.prototype.toJSON = function () {
-  return this.toString(); // Convert BigInt to String for JSON serialization
+  return this.toString();  // Convert BigInt to String to serialize it
 };
