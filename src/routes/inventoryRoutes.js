b453f672466ebb121fb6ebb82c11ab7d3eda596c
Edwin Allauca <edwindavid21v@gmail.com>
2025-02-02 16:38:40 -0500

    feat: :sparkles: Add Middlewares folder and jwt configurations
    Add new files and configurations with jwt

--- src/routes/inventoryRoutes.js
+++ src/routes/inventoryRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const inventoryController = require('../controllers/inventoryController');
+const { authorize } = require('../Middlewares/authMiddleware');
 
-router.post('/register', inventoryController.registerInventory);
+router.post('/register', inventoryController.registerInventory, authorize);
 
 module.exports = router;
