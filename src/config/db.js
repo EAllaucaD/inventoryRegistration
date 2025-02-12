038885ff52df208f6beeaf3dc918e446d423d371
Edwin Allauca <edwindavid21v@gmail.com>
2025-02-11 22:59:08 -0500

    Add new configurations for connection DB. (#6)
    * feat: :wrench: Add files
    
    Add new files
    
    * feat: :sparkles: Add new features
    
    Add new lines in app js and use cors
    
    * feat: :wrench: Add files for swagger
    
    Add files for swagger and update app.js file
    
    * fix: :bug: Fix in ci.yml
    
    Fix in ci.yml and update the workflow.
    
    * feat: :art: Add readme
    
    Add readme with changes for this microservice create
    
    * feat: :sparkles: Add Middlewares folder and jwt configurations
    
    Add new files and configurations with jwt
    
    * feat: :sparkles: Add cd.yml
    
    Aadd new cd.yml and configurations
    
    * feat: :bug: Fix a bug in cd.yml
    
    Update configurations in cd.yml
    
    * feat: :bug: Fix a bug
    
    Fix a bun in cd.yml
    
    * feat: :bug: Fix a bug
    
    Fix a bung in cd.yml
    
    * feat: :bug: Update a changes
    
    Update a changes in cd.yml
    
    * feat: :memo: Update and changes in yml
    
    Add new configurations in yml
    
    * feat: :bug: Fix in a yml file
    
    Fix a bun in yml file and configurations
    
    * feat: :bug: Fix a big
    
    Fix a bug
    
    * feat: :memo: Update yml
    
    Update changes
    
    * feat: :bug: Fix a bug
    
    Fix a bug
    
    * feat: :wrench: Update configurations
    
    Uodate configurations
    
    * feat: :wrench: Update configurations
    
    Update new configurations
    
    ---------
    
    Co-authored-by: Sebastián Carrera <90810854+Retrofiyer@users.noreply.github.com>

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,21 @@ require('dotenv').config();
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: process.env.HOST_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE_NAME,
 });
+
+async function testConnection() {
+  try {
+    const res = await pool.query('SELECT NOW()');
+    console.log("Connection successful:", res);
+  } catch (err) {
+    console.error("Connection error:", err);
+  }
+}
+
+testConnection();
 
 module.exports = pool;
